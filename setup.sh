553e2d5d0a1d5da075993458865b2b2b03b92f92
Brandon <willjasen@gmail.com>
2017-11-23 21:10:29 -0500

    don't run lots

--- setup.sh
+++ setup.sh
@@ -23,39 +23,39 @@ export LC_ALL=C
 # (insert into /etc/fstab) /dev/sda1	/media/sync	ext4	defaults	0	0
 
 # Setup 802.1q support
-sudo apt-get install vlan
-echo '8021q' | sudo tee /etc/modules
-sudo modprobe 8021q
-sudo /etc/init.d/networking restart
+# sudo apt-get install vlan
+# echo '8021q' | sudo tee /etc/modules
+# sudo modprobe 8021q
+# sudo /etc/init.d/networking restart
 
 # Install and run BitTorrent Sync
-mkdir ~/.btsync
-curl -o ~/.btsync/BitTorrent-Sync_arm.tar.gz https://download-cdn.getsync.com/stable/linux-arm/BitTorrent-Sync_arm.tar.gz
-tar xfv ~/.btsync/BitTorrent-Sync_arm.tar.gz -C ~/.btsync
-sudo cp btsync /etc/init.d
-cp btsync.conf ~/.btsync
-sudo chmod 755 /etc/init.d/btsync
-sudo update-rc.d btsync defaults
-sudo service btsync start
+# mkdir ~/.btsync
+# curl -o ~/.btsync/BitTorrent-Sync_arm.tar.gz https://download-cdn.getsync.com/stable/linux-arm/BitTorrent-Sync_arm.tar.gz
+# tar xfv ~/.btsync/BitTorrent-Sync_arm.tar.gz -C ~/.btsync
+# sudo cp btsync /etc/init.d
+# cp btsync.conf ~/.btsync
+# sudo chmod 755 /etc/init.d/btsync
+# sudo update-rc.d btsync defaults
+# sudo service btsync start
 
 # Update available packages
 sudo apt-get -y update
 
 # Install needed packages
-sudo apt-get install -y git curl zlib1g-dev
-sudo apt-get install -y openssl libreadline6-dev git-core zlib1g libssl-dev
-sudo apt-get install -y libyaml-dev libsqlite3-dev sqlite3 postgresql
-sudo apt-get install -y libxml2-dev libxslt-dev
-sudo apt-get install -y autoconf automake libtool bison
-sudo apt-get install -y nodejs libpq-dev
+# sudo apt-get install -y git curl zlib1g-dev
+# sudo apt-get install -y openssl libreadline6-dev git-core zlib1g libssl-dev
+# sudo apt-get install -y libyaml-dev libsqlite3-dev sqlite3 postgresql
+# sudo apt-get install -y libxml2-dev libxslt-dev
+# sudo apt-get install -y autoconf automake libtool bison
+# sudo apt-get install -y nodejs libpq-dev
 
 # Install RVM
-curl -L get.rvm.io | bash -s stable --rails
-source ~/.rvm/scripts/rvm
+# curl -L get.rvm.io | bash -s stable --rails
+# source ~/.rvm/scripts/rvm
 
-echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-gem install bundler
-gem install execjs
+# echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+# gem install bundler
+# gem install execjs
 
 # Configure git
 git config --global color.ui true
