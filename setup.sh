e9922923e2bf83134b32e6415b1cfb4f9d9af2dd
Brandon <willjasen@gmail.com>
2017-11-23 22:47:38 -0500

    setup encrypted RAID 0 across two USB flash drives

--- setup.sh
+++ setup.sh
@@ -14,13 +14,7 @@ sudo apt-get -y update
 
 # Install needed packages
 echo "Installing packages.."
-sudo apt-get git mdadm
-# sudo apt-get install -y git curl zlib1g-dev
-# sudo apt-get install -y openssl libreadline6-dev git-core zlib1g libssl-dev
-# sudo apt-get install -y libyaml-dev libsqlite3-dev sqlite3 postgresql
-# sudo apt-get install -y libxml2-dev libxslt-dev
-# sudo apt-get install -y autoconf automake libtool bison
-# sudo apt-get install -y nodejs libpq-dev
+sudo apt-get git mdadm cryptsetup
 
 # Configure git
 echo "Setting up git.."
@@ -28,18 +22,25 @@ git config --global color.ui true
 git config --global user.name willjasen
 git config --global user.email willjasen@gmail.com
 
-# Setup RAID 0 span
-echo "Setting up RAID 0"
+# Setup encrypted RAID 0 span
+echo "Setting up encrypted RAID 0 as /mnt/secure"
 sudo mdadm --create /dev/md0 --level=0 --raid-devices=2 /dev/sda1 /dev/sdb1
-sudo mkdir -p /mnt/raid0
+sudo mkdir -p /mnt/md0
 sudo mkfs.ext4 /dev/md0
-sudo mount /dev/md0 /mnt/raid1/
+sudo modprobe dm-crypt sha256 aes
+sudo cryptsetup --verify-passphrase luksFormat /dev/md0 -c aes -s 256 -h sha256
+sudo cryptsetup luksOpen /dev/md0 raid0
+sudo mkfs -t ext4 -m 1 /dev/mapper/raid0
+sudo mkdir /mnt/secure
+sudo mount /dev/mapper/raid0 /mnt/secure/
+sudo chown pi:pi /mnt/secure/
+df -h | grep /mnt/secure
 
 # Setup SSH with public key
 echo "Setting up SSH with public key"
-mkdir .ssh
-curl -o .ssh/authorized_keys https://willjasen.keybase.pub/authorized_keys
-chmod 600 .ssh/authorized_keys
+mkdir ~/.ssh
+curl -o ~/.ssh/authorized_keys https://willjasen.keybase.pub/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
 
 # Update the password for pi
 echo "Change the password for user pi"
